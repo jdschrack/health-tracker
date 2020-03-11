3cc0dc4192b9bf5c9d65c4638181dc3983657611
Jeremey Schrack <jeremey@securegive.com>
2020-03-11 18:19:43 -0400

    starting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/appStore';
+import Theme from './components/theme/Theme';
+
+import AppRouter from './components/router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Theme>
+        <AppRouter />
+      </Theme>
+    </Provider>
   );
 }
 
